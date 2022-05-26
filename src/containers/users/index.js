16b31f050f9624a17599fd75eec68268c71c44ab
Kevin Campos <kevinmanx4@hotmail.com>
2022-05-26 03:07:38 -0600

    [ADD] Consumiendo servicio para obtener usuarios y maquetando cards de usuarios en ruta /users

--- src/containers/users/index.js
+++ src/containers/users/index.js
@@ -1,15 +1,87 @@
-import React, { memo } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import {
     MainContainer,
     Card,
+    UserCardsContainer,
+    UserCard,
+    UserFullName,
+    UserEmail,
+    ButtonsContainer,
 } from './styles';
+import axios from '../../config/https';
+import { enviroment } from '../../config/EnviromentConfig';
+import { get } from 'lodash';
+import { Divider } from '../../utils/styled-components';
+import { Button } from '@mui/material';
+import { useNavigate  } from 'react-router-dom';
 
 function Users() {
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const navigate =useNavigate();
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = () => {
+        axios.get(enviroment.API_ENDPOINT_URL + `api/users?page=${page}`)
+            .then(response => {
+                const responseUsers = get(response, 'data.data', []);
+                const allUsers = users.concat(responseUsers);
+
+                setUsers(allUsers);
+                setPage(page + 1);
+            }).catch(error => {
+            console.log("error", error);
+        });
+    };
+
+    const handleLogOut = () => {
+        localStorage.removeItem('user');
+        navigate('/');
+    };
 
     return (
         <MainContainer>
             <Card>
-                una routa
+                <UserCardsContainer>
+                    {users && users.map(_user => (
+                        <UserCard
+                            key={`${get(_user, 'avatar', '')}${get(_user, 'first_name', '')} `}
+                        >
+                            <img
+                                src={get(_user, 'avatar', '')}
+                                alt="get(_user, 'email', '')"
+                                className="avatar-img"
+                            />
+                            <UserFullName>
+                                {get(_user, 'first_name', '')} {get(_user, 'last_name', '')}
+                            </UserFullName>
+                            <Divider size={4} />
+                            <UserEmail>
+                                {get(_user, 'email', '')}
+                            </UserEmail>
+                        </UserCard>
+                    ))}
+                </UserCardsContainer>
+                <ButtonsContainer>
+                    <Button
+                        variant="contained"
+                        disabled={page > 2}
+                        onClick={()  => fetchUsers()}
+                    >
+                        Cargar más
+                    </Button>
+                    <Button
+                        variant="text"
+                        className="sign-out-button"
+                        onClick={()  => handleLogOut()}
+                    >
+                        Cerrar sesión
+                    </Button>
+                </ButtonsContainer>
             </Card>
         </MainContainer>
     );
--- src/containers/users/styles.js
+++ src/containers/users/styles.js
@@ -14,11 +14,66 @@ export const Card = styled.div`
   width: 50%;
   height: 80%;
   background-color: #ffffff;
+  padding: 16px;
   border-radius: 16px;
-  display: flex;
   
   ${mediaQuery} {
     width: 100%;
     height: 100%;
   }
+`;
+
+export const UserCardsContainer = styled.div`
+  height: 90%;
+  overflow-y: auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 12px;
+  
+  ${mediaQuery} {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  
+  & .sign-out-button {
+    background-color: brown;
+    color: white;
+  }
+`;
+
+export const UserCard = styled.div`
+  width: 150px;
+  height: 140px;
+  padding: 12px 24px;
+  background-color: #fcfcfc;
+  border-radius: 8px;
+  border: 1px solid lightgray;
+  margin-right: 8px;
+  margin-bottom: 12px;
+  user-select: none;
+
+  & .avatar-img {
+    width: 80px;
+    height: 80px;
+    border-radius: 40px;
+  }
+`;
+
+export const UserFullName = styled.div`
+  width: 100%;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const UserEmail = styled.div`
+  width: 100%;
+  font-size: 14px;
+  letter-spacing: -0.2px;
 `;
